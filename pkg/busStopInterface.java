209c38b7ef0458927aeeb529d5fca36ac869812a
Niamh0307 <102161425+Niamh0307@users.noreply.github.com>
2022-03-28 16:35:15 +0100

    3

--- pkg/busStopInterface.java
+++ pkg/busStopInterface.java
@@ -4,5 +4,5 @@ public interface busStopInterface {
 public void shortestPath(int startingStop, int finalStop);
 public void stopName(String name);
 public void intervalTimeTrips(Date startTime, Date endTime);
-public void pushGit();
+public void pushGit1();
 }
